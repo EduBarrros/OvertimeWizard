d8c6d35d698af61b843490d0136719e698cfe613
Eduardo de Oliveira Barros <eduardodeoliveirabarros@gmail.com>
2024-03-07 16:05:37 -0300

    Feat: add react navigation stack and header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Header = () => {
+    return(
+        <>
+            <Text>
+                Header
+            </Text>
+        </>
+    )
+};
+
+export default Header;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, SafeAreaView} from "react-native";
+
+import Header from "../../components/Header";
+
+const Home = () => {
+    return(
+        <SafeAreaView>
+           <Header />
+        </SafeAreaView>
+    )
+};
+
+export default Home;
